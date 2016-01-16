ec5afaf318e9118b3d0155da1b9bae69c5b085cc
Juho Kilpikoski <juho@teelisyys.fi>
2016-01-16 13:55:52 +0100

    Extend wikimarkup-plaintext-converter

--- src/main/java/fi/teelisyys/tekiel/WikiTextUtils.java
+++ src/main/java/fi/teelisyys/tekiel/WikiTextUtils.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.*;
 import static java.util.regex.Pattern.UNICODE_CHARACTER_CLASS;
 import static java.util.regex.Pattern.compile;
 
@@ -57,16 +58,42 @@ public class WikiTextUtils {
         // refs
         wikiText = gsub(wikiText, compile("</?ref[^>]*>", UNICODE_CHARACTER_CLASS), REMOVE);
 
-        // links
+        wikiText = gsub(wikiText, compile("</?su[pb]>", UNICODE_CHARACTER_CLASS), REMOVE);
+
+        // external links
         wikiText = gsub(wikiText, compile("\\[(?<url>http[^ \\]]+) (?<text>[^\\]]+)\\]", UNICODE_CHARACTER_CLASS), m -> m.group("text"));
 
 
-        Pattern templatePattern = compile("\\{\\{[^\\{\\}]*\\}\\}", (UNICODE_CHARACTER_CLASS | Pattern.MULTILINE));
+        // Tables
+        Pattern tablePattern = compile("\\{\\|[^\\}]*\\|\\}", (UNICODE_CHARACTER_CLASS | MULTILINE));
+        wikiText = gsub(wikiText, tablePattern, REMOVE);
+
+
+        Pattern templatePattern = compile("\\{\\{[^\\{\\}]*\\}\\}", (UNICODE_CHARACTER_CLASS | MULTILINE));
 
         wikiText = gsub(wikiText, templatePattern, REMOVE);
         wikiText = gsub(wikiText, templatePattern, REMOVE); // Nested templates
         wikiText = gsub(wikiText, templatePattern, REMOVE); // Nested templates
 
+        // Tables after links
+        wikiText = gsub(wikiText, tablePattern, REMOVE);
+
+        // {.*} is not interesting
+        wikiText = gsub(wikiText, compile("\\{[^\\}]*\\}", (UNICODE_CHARACTER_CLASS | MULTILINE)), REMOVE);
+
+        // foo=bar is not interesting
+        wikiText = gsub(wikiText, compile("[a-z]+=\"[a-z]+\"", (UNICODE_CHARACTER_CLASS | MULTILINE | Pattern.CASE_INSENSITIVE)), REMOVE);
+
+
+
+        // Math markup
+        wikiText = gsub(wikiText, compile("<math[^<]*</math>", (UNICODE_CHARACTER_CLASS | MULTILINE)), REMOVE);
+
+        // html-tags
+        wikiText = gsub(wikiText, compile("</?[a-z][^>]*>>", (UNICODE_CHARACTER_CLASS | MULTILINE)), REMOVE);
+
+
+        wikiText = gsub(wikiText, compile("&nbsp;", UNICODE_CHARACTER_CLASS ), m -> " ");
         return wikiText;
     }
 }
